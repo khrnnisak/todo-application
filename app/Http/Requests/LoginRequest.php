18efb6b3a35170a0facb62dafd603195bb57ef15
khrnnisak <khrnnisak07@gmail.com>
2022-06-12 21:28:20 +0700

    pembuatan fitur register dan login

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class LoginRequest extends FormRequest
+class LoginRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
      */
+
     public function authorize()
     {
         return true;
@@ -24,8 +23,8 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|string|email',
-            'password' => 'required|string\min:8',
+            'email'     => 'required|string|email',
+            'password'  => 'required|string|min:8'
         ];
     }
 }
